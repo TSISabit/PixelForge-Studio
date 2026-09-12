252a436ffd8d433dd5196e8fe02446153ffd808a
TSISabit <tariksaidulislam@gmail.com>
2026-09-12 15:47:09 +0600

    Final Commit: Added Scripts for popup text, fixed stytles and structures for popup text

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Hello")
+function showPopUp(){
+    // console.log("Function Called"); 
+    let box = document.getElementById('popupBox')
+    // console.log(box); 
+    if(box.style.display == 'none'){
+        box.style.display = 'block'; 
+    }
+    else{
+        box.style.display = 'none'; 
+    }
+}
